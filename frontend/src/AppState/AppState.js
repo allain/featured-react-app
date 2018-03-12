f4d6aeef092701c9df88c50b7aa47a9bb5bcf6e4
Allain Lalonde <allain.lalonde@gmail.com>
2018-03-12 11:19:08 -0400

    App State cleanup

--- frontend/src/AppState/AppState.js
+++ frontend/src/AppState/AppState.js
@@ -1,16 +1,7 @@
 import ObservableState from 'observable-react-state'
 
 class AppState extends ObservableState {
-  constructor({ history } = {}) {
-    super()
-    this.state = {
-      history
-    }
-  }
-
-  redirect(path, push = true) {
-    this.state.history[push ? 'push' : 'replace'](path)
-  }
+  state = {}
 }
 
 export default AppState
